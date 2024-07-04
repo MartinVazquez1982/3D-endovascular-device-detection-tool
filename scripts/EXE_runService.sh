a052404277f5abda087c9d54eff1a25dd08304d5
MartinVazquez1982 <martin.vazquezaris@gmail.com>
2024-07-04 10:16:52 -0300

    :fire: script directory
    because we didn't use it

--- scripts/EXE_runService.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --time=00:60:00
-#SBATCH --nodes=1
-#SBATCH --mem=5GB
-#SBATCH --job-name=inference
-#SBATCH --output=inference.out
-
-nnUNetv2_predict_from_modelfolder -i '/home/mvazquez/frontend/source_user_image/user_image.nii.gz' -o '/home/mvazquez/frontend/source_inference' -m "/home/mvazquez/model" -f 0
